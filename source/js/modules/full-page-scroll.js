8ffaabb6db86d1d3c45b78257cf155547da3eb7b
urchifox <126313096+urchifox@users.noreply.github.com>
2025-01-09 22:06:41 +0400

    Revert "add gametimer"
    This reverts commit 99f203c806c138d588521424e382d04b36d7da6b.

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,6 +1,5 @@
 import throttle from 'lodash/throttle';
 import runSvgAnimation from './runSvgAnimation';
-import gameTimer from './game-timer';
 
 export default class FullPageScroll {
   constructor() {
@@ -59,11 +58,6 @@ export default class FullPageScroll {
       runSvgAnimation(document.querySelector(`#prize1svg`), 1);
       runSvgAnimation(document.querySelector(`#prize2svg`), 4000);
       runSvgAnimation(document.querySelector(`#prize3svg`), 7000);
-    }
-
-    if (this.screenElements[this.activeScreen].id === `game`) {
-      // gameTimer(10 * 1000, document.querySelector(`.game__counter`));
-      gameTimer(5 * 60 * 1000, document.querySelector(`.game__counter`));
     }
   }
 
